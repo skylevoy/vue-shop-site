6f549d4f4c5f00ebb9fe8401892959acf8de7af4
vholcomb78 <vholcomb78@gmail.com>
2025-02-14 09:35:14 -0800

    Changes part 1

---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Shopping Cart Page</h1>
+    <div v-if="cartItems.length > 0">
+        <div 
+            class="product-container"
+            v-for="product in cartItems"
+            :key="product.id"
+        >
+            <img class="product-image" :src="product.imageName" alt="Product Image" />
+            <div class="details-wrap">
+                <h3>{{ product.name }}</h3>
+                <p>{{ product.price }}</p>
+            </div>
+            <button class="remove-button">Remove from Cart</button>
+        </div>
+        <button class="checkout-button">Proceed to Checkout</button>
+    </div>
+    <div v-if="cartItems.length === 0">
+        <h2>Your cart is empty</h2>
+    </div>
+</template>
+
+<script>
+import { cartItems } from '../temp-data';
+
+export default {
+    name: 'ShoppingCartPage',
+    data() {
+        return {
+            cartItems,
+        }
+    },
+}
+</script>
