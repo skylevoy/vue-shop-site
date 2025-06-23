df7aedf1b8fafea2fe9f525c1d441a3c274fdebe
Steven Kyle <steven.kyle@gcomsoft.com>
2025-06-23 17:11:44 -0500

    Testing adding LaunchDarkly

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -6,9 +6,20 @@ import ShoppingCartPage from './pages/ShoppingCartPage.vue';
 import ProductsPage from './pages/ProductsPage.vue';
 import ProductDetailPage from './pages/ProductDetailPage.vue';
 import NotFoundPage from './pages/NotFoundPage.vue';
+import { LDPlugin } from "launchdarkly-vue-client-sdk"
 
-createApp(App)
-.use(VueRouter.createRouter({
+const app = createApp(App)
+
+// TODO inject via env variables
+app.use(LDPlugin, {
+  clientSideID: "68519beb63a99d09011f3f5c",
+  context: {
+    kind: "user",
+    key: "anonymous-user",
+  },
+})
+
+app.use(VueRouter.createRouter({
     history: VueRouter.createWebHistory(process.env.BASE_URL),
     routes: [
         {
--- front-end/src/pages/ProductsPage.vue
+++ front-end/src/pages/ProductsPage.vue
@@ -1,9 +1,31 @@
 <template>
     <h1>Products</h1>
     <ProductsList :products="products" />
+    <div v-if="saveDraftButton">TESTING TEXT PROBS CHANGE LATER</div>
 </template>
 
 <script>
+/* eslint-disable */
+import { useLDClient } from 'launchdarkly-vue-client-sdk';
+
+export default {
+    data() {
+        return {
+            saveDraftButton: null,
+        };
+    },
+    async mounted() {
+        const ldClient = useLDClient();
+
+        ldClient.on('ready', () => {
+            this.saveDraftButton = ldClient.variation('save-draft-button', false);
+        });
+        ldClient.on('change', () => {
+            this.saveDraftButton = ldClient.variation('save-draft-button', false);
+        });
+    },
+};
+/*
 import axios from 'axios';
 import ProductsList from '../components/ProductsList.vue';
 
@@ -23,4 +45,5 @@ export default {
         this.products = products;
     }
 }
+*/
 </script>
